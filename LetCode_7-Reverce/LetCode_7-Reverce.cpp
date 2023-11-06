fa5af122689e18a107a5865f62a139f5d405b1a0
Nuta <111691010+Anikalogika4417@users.noreply.github.com>
2023-11-06 20:52:30 +0200

    Add files via upload

---
+++ LetCode_7-Reverce/LetCode_7-Reverce.cpp
@@ -0,0 +1,16 @@
+// LetCode_7-Reverce.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+class Solution {
+public:
+    int reverse(int x) {
+
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
