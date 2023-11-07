320653c231efb7a34f79277471dcf42e76f7987f
Anikalogika4417 <anikalogika4417@gmail.com>
2023-11-07 22:01:31 +0200

    finish

--- LetCode_7-Reverce/LetCode_7-Reverce.cpp
+++ LetCode_7-Reverce/LetCode_7-Reverce.cpp
@@ -1,16 +1,36 @@
 // LetCode_7-Reverce.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <algorithm> 
+#include<iostream>
+#include<string>
+
+using namespace std;
 
 class Solution {
 public:
     int reverse(int x) {
+        if (x == 0) return 0;
+        string str = to_string(x);
+        short is_neg;
+        is_neg = str[0] == '-' ? -1 : 1;
+        std::reverse(str.begin(), str.end());
+
+        while (str[0] == '0') {
+            str.erase(0, 1);
+        }
+        try{
+            return is_neg * stoi(str);
+        }
+        catch (out_of_range const&) {
+            return 0;
+        }
 
     }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution test;
+    cout << test.reverse(1534236469);
 }
